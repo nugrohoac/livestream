31dcf16419545c8426eaa81354f199a723b904c7
nugrohoac96 <cahyono_na@pahamify.com>
2021-01-25 09:09:32 +0700

    feat: init boiler plate folder and file

---
+++ interface/livestream.go
@@ -0,0 +1,9 @@
+package _interface
+
+// LivestreamService is interface for live stream service at service/livestream
+type LivestreamService interface {
+}
+
+// LiveStreamRepository is interface for data source of live stream at repository for all data source, mysql, http,
+type LiveStreamRepository interface {
+}
